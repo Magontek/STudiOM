96e6c711f313458d74ac45ae29400ef9bfbafaed
Timmy <timmy.burton@hotmail.com>
2020-10-14 00:09:20 -0300

    Floating Texts
    textos flotantes, se invoca con Q, muere en 1f

---
+++ test1/Assets/Scripts/TEST_textonplayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_textonplayer : MonoBehaviour
+{
+
+	public GameObject Floating_TextPrefab;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			if(Floating_TextPrefab)
+			{
+				ShowFloatingText();
+			}
+		}
+	}
+
+	void ShowFloatingText()
+	{
+		Instantiate(Floating_TextPrefab, transform.position, Quaternion.identity, transform);
+	}
+}
