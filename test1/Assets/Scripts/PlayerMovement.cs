848f14e6e34e53c29fa1955b08cdf4a6c5db368f
Drupus <timmy.burton@hotmail.com>
2020-10-14 14:25:14 -0300

    Re-Fix

---
+++ test1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] AnimationCurve curveY = null;
+	[SerializeField] float speed = 6f;
+	Rigidbody2D rb;
+	Vector2 movement;
+	Vector2 currentPos;
+	Vector2 landingPos;
+	float landingDis;
+	//float speed = 1f;
+	float timeElapsed = 0f;
+	bool onGround = true;
+	bool jump = false;
+    // Start is called before the first frame update
+    public Animator animator;
+    void Start()
+    {
+    	rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+    	InputHandler();
+    }
+
+    void FixedUpdate()
+    {
+    	if(jump)
+    	{
+    		JumpHandler();
+    		animator.SetBool("Jumping", true);
+    	}
+    	else
+    	{
+    		animator.SetBool("Jumping", false);
+    		MovementHandler();
+    		if(Mathf.Abs(movement.x) > 0|| Mathf.Abs(movement.y) > 0)
+    		{
+    			animator.SetBool("Walking", true);
+    		}
+    		else
+    		{
+    			animator.SetBool("Walking", false);
+    		}
+    	}
+    }
+
+    void JumpHandler()
+    {
+    	if(onGround)
+    	{
+    		currentPos = rb.position;
+    		landingPos = currentPos + movement.normalized * speed;
+    		landingDis = Vector2.Distance(landingPos, currentPos);
+    		timeElapsed = 0f;
+    		onGround = false;
+    	}
+    	else
+    	{
+    		timeElapsed += Time.fixedDeltaTime * speed / landingDis;
+    		if(timeElapsed <= 1f)
+    		{
+    			currentPos = Vector2.MoveTowards(currentPos, landingPos, Time.fixedDeltaTime * speed);
+    			rb.MovePosition(new Vector2(currentPos.x, currentPos.y + curveY.Evaluate(timeElapsed)));
+    		}
+    		else
+    		{
+    			jump = false;
+    			onGround = true;
+    		}
+    	}
+    }
+
+    void MovementHandler()
+    {
+    	rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void InputHandler()
+    {
+    	float horizontal = Input.GetAxis("Horizontal");
+    	float vertical = Input.GetAxis("Vertical");
+
+
+    	movement = new Vector2(horizontal, vertical);
+
+    	if(Input.GetKeyDown("space"))
+    	{
+    		jump = true;
+    	}
+    }
+}
---
+++ test1/Assets/Scripts/TEST_textonplayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Este script va en la entidad
+
+public class TEST_textonplayer : MonoBehaviour
+{
+
+	public GameObject Floating_TextPrefab;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))//ESTE ES EL TRIGGER puede ser lo q sea
+		{
+			if(Floating_TextPrefab)//PRIMERO COMPRUEBA Q EXISTA (asi decia la instruccion)
+			{
+				ShowFloatingText();//Invoca al texto
+			}
+		}
+	}
+
+	
+	void ShowFloatingText()//Funcion de crear el texto de un PREFAB
+	{
+		var go = Instantiate(Floating_TextPrefab, transform.position, Quaternion.identity, transform);
+		go.GetComponent<TextMesh>().text = "SOY TEXTO";//ACA se decide q dice el texto, puede ser de una variable
+	}
+}
