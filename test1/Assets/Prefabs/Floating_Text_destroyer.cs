9edf54c995cc92cb5c4dba76fc37f324cf358aa0
Timmy <timmy.burton@hotmail.com>
2020-10-14 00:23:11 -0300

    Floating texts Descripción
    Mirar Assets/Prefabs/Floating_Text_destroyer
    para saber más

--- test1/Assets/Prefabs/Floating_Text_destroyer.cs
+++ test1/Assets/Prefabs/Floating_Text_destroyer.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*El prefab y este script dependen de una entidad teniendo un trigger en su script
+que activa el texto... ver "Assets/Scripts/TEST_textonplayer" para saber mas*/
 public class Floating_Text_destroyer : MonoBehaviour
 {
 
     public float DestroyTime = 3f;
 
-    void Start()
+    void Start()//DESTRUYE EL TEXTo despues de DestroyTime
     {
         Destroy(gameObject, DestroyTime);
     }
