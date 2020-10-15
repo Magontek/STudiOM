6bcd44cdda70b04b246f6fda1077f3c0c026b576
Drupus <timmy.burton@hotmail.com>
2020-10-14 21:41:52 -0300

    Movimiento v2
    sin saltos, velocidad ajustada, y sigue las paralelas en "diagonal"

--- test1/Assets/Scripts/PlayerMovement.cs
+++ test1/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-	[SerializeField] AnimationCurve curveY = null;
-	[SerializeField] float speed = 6f;
+	float speed = 1f;
 	Rigidbody2D rb;
 	Vector2 movement;
-	Vector2 currentPos;
-	Vector2 landingPos;
-	float landingDis;
-	//float speed = 1f;
-	float timeElapsed = 0f;
-	bool onGround = true;
-	bool jump = false;
-    // Start is called before the first frame update
     public Animator animator;
     void Start()
     {
@@ -25,71 +16,38 @@ public class PlayerMovement : MonoBehaviour
     void Update(){
     	InputHandler();
     }
-
+    
     void FixedUpdate()
     {
-    	if(jump)
-    	{
-    		JumpHandler();
-    		animator.SetBool("Jumping", true);
-    	}
-    	else
-    	{
-    		animator.SetBool("Jumping", false);
-    		MovementHandler();
-    		if(Mathf.Abs(movement.x) > 0|| Mathf.Abs(movement.y) > 0)
-    		{
-    			animator.SetBool("Walking", true);
-    		}
-    		else
-    		{
-    			animator.SetBool("Walking", false);
-    		}
-    	}
-    }
-
-    void JumpHandler()
-    {
-    	if(onGround)
-    	{
-    		currentPos = rb.position;
-    		landingPos = currentPos + movement.normalized * speed;
-    		landingDis = Vector2.Distance(landingPos, currentPos);
-    		timeElapsed = 0f;
-    		onGround = false;
-    	}
-    	else
-    	{
-    		timeElapsed += Time.fixedDeltaTime * speed / landingDis;
-    		if(timeElapsed <= 1f)
-    		{
-    			currentPos = Vector2.MoveTowards(currentPos, landingPos, Time.fixedDeltaTime * speed);
-    			rb.MovePosition(new Vector2(currentPos.x, currentPos.y + curveY.Evaluate(timeElapsed)));
-    		}
-    		else
-    		{
-    			jump = false;
-    			onGround = true;
-    		}
-    	}
+		MovementHandler();
+		if(Mathf.Abs(movement.x) > 0|| Mathf.Abs(movement.y) > 0)
+		{
+			animator.SetBool("Walking", true);
+		}
+		else
+		{
+			animator.SetBool("Walking", false);
+		}
     }
 
     void MovementHandler()
     {
-    	rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    	rb.MovePosition(rb.position + movement * speed);
+    	//rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 
     void InputHandler()
     {
     	float horizontal = Input.GetAxis("Horizontal");
     	float vertical = Input.GetAxis("Vertical");
-
-
-    	movement = new Vector2(horizontal, vertical);
-
-    	if(Input.GetKeyDown("space"))
+    	if (horizontal != 0 && vertical != 0)
     	{
-    		jump = true;
+    		movement = new Vector2(horizontal/29, vertical/50)/15*10;
     	}
+    	else
+    	{
+    	movement = new Vector2(horizontal, vertical)/40;
+    	}
+
     }
 }
