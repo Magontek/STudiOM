fcaa1838187b9bed0454df91ee73223bf60d4161
Drupus <timmy.burton@hotmail.com>
2020-10-16 02:36:12 -0300

    Console Log clear: en-AU

--- test1/Assets/Scripts/Menu_Principal.cs
+++ test1/Assets/Scripts/Menu_Principal.cs
@@ -7,7 +7,8 @@ public class Menu_Principal : MonoBehaviour
 {
 	public void PlayGame()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		SceneManager.LoadScene(1);
 	}
 
 	public void QuitGame()
