1cf0a30a711902e1f0483d816df731e6a28c31b9
Drupus <timmy.burton@hotmail.com>
2020-10-20 19:25:42 -0300

    Prueba creador de npc random
    ver objeto event gamestarter

--- test1/Assets/Scripts/NameMaker.cs
+++ test1/Assets/Scripts/NameMaker.cs
@@ -7,16 +7,25 @@ public class NameMaker : MonoBehaviour
 
 	public GameObject NPCPrefab;
 
+
     void Start()
     {
-        Name();
+    	int i;
+    	int max = Random.Range(100,1000);
+    	for(i=0;i<max;i++)
+    	{
+        	Name();
+        }
     }
  
 
  	void Name ()
  	{
- 		Instantiate(NPCPrefab, transform.position, Quaternion.identity, transform);
- 		int KEY = Random.Range(0, 4);
+ 		int x = Random.Range(-5, 10);
+ 		int y = Random.Range(-10, 0);
+ 		var random_post = new Vector3(x,y,0);
+ 		Instantiate(NPCPrefab, random_post, Quaternion.identity, transform);
+ 		
  	}
 
 }
