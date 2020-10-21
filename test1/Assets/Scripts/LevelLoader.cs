1eed694f778dcd241efe72c8e629461bc3fed0ee
Drupus <timmy.burton@hotmail.com>
2020-10-21 19:12:00 -0300

    Level Loader
    Cargador de niveles asincrónico, ver objeto LevelLoader al comienzo de menu

---
+++ test1/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+	public GameObject loadingScreen;
+	public Slider slider;
+	public TMPro.TextMeshProUGUI progressText;
+
+	public void LoadLevel (int scIndex)
+	{
+		StartCoroutine(LoadAsynchronously(scIndex));
+	}
+
+	IEnumerator LoadAsynchronously (int scIndex)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(scIndex);
+
+		loadingScreen.SetActive(true);
+
+		while (!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / .9f);
+			slider.value = progress;
+			progressText.text = progress *100f + "%";
+			yield return null;
+		}
+	}
+}
