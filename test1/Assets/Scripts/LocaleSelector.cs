15dbcf84ed481b9058e27c45cb92813513804c4e
Drupus <timmy.burton@hotmail.com>
2020-10-23 15:33:20 -0300

    Lluvia v2.1 | Merge de Camera Scripts | Retoques menu pausa

--- test1/Assets/Scripts/LocaleSelector.cs
+++ test1/Assets/Scripts/LocaleSelector.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-using UnityEngine.Localization;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Localization;
+using System.Collections.Generic;
 using UnityEngine.Localization.Settings;
 
 public class LocaleSelector : MonoBehaviour
