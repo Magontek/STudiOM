797039e49d4a3a2befd1279a755f85c77975f51a
Drupus <timmy.burton@hotmail.com>
2020-10-23 12:44:40 -0300

    Lluvia v1 (no se ajusta)

--- test1/Assets/Scripts/CameraZoomer.cs
+++ test1/Assets/Scripts/CameraZoomer.cs
@@ -11,6 +11,7 @@ public class CameraZoomer : MonoBehaviour
     {
     	cam = Camera.main;
 		targetZoom = cam.orthographicSize;
+		ParticleSystem rain = GetComponent<ParticleSystem>();
     }
 
 
@@ -18,7 +19,7 @@ public class CameraZoomer : MonoBehaviour
 	{
 
 		float ScrollWheelChange;
-		ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");  
+		ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
 		targetZoom -= ScrollWheelChange * 3f; // Cantidad de zoom por rueda
 		targetZoom = Mathf.Clamp(targetZoom, 2f, 8f); // LIMITES DE LA CAMARA
 		cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * 10);
