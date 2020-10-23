4560103ba8c3c8acb1fb3ac53745c80b1e9a95ea
Drupus <timmy.burton@hotmail.com>
2020-10-23 20:34:57 -0300

    Arreglos y detalles en menúes

--- test1/Assets/Scripts/opciones.cs
+++ test1/Assets/Scripts/opciones.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using UnityEngine.Localization.Settings;
 
 public class opciones : MonoBehaviour
 {
 	public AudioMixer audioMixer;
 	public Slider slider;
-	public string Language;
 
 	public void Awake ()
 	{
@@ -23,33 +21,9 @@ public class opciones : MonoBehaviour
 		}
 	}
 
-	public void Start ()
-	{
-		var localizationSettings = LocalizationSettings.GetInstanceDontCreateDefault();
-		if (PlayerPrefs.HasKey("Language"))
-		{
-			Language = PlayerPrefs.GetString("Language");
-			localizationSettings = ScriptableObject.CreateInstance<LocalizationSettings>();
-			LocalizationSettings.Instance = localizationSettings;
-		}
-		else
-		{
-			PlayerPrefs.SetString("Language", "English");
-
-		}
-	}
-
 	public void SetVolume (float slider_value)
 	{
 		PlayerPrefs.SetFloat("Volume", slider_value);
 		audioMixer.SetFloat("Volume_Master", slider_value);
-	}
-
-	public void SetLanguage (string language)
-	{ /*
-		var localizationSettings = LocalizationSettings.GetInstanceDontCreateDefault(); // NO SE Q ESTOY HACIENDO
-		localizationSettings = ScriptableObject.CreateInstance<LocalizationSettings>();
-		//LocalizationSettings.Instance = localizationSettings;
-		LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[3];       */
 	}
 }
